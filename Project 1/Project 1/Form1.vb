5bd1bc335906f0e645a94323873ebe8226bfd70e
huddaamin <hudda.amin@gmail.com>
2017-03-14 13:53:56 -0400

    Test Files

---
+++ Project 1/Project 1/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello World")
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label1_MouseHover(sender As Object, e As EventArgs) Handles Label1.MouseHover, Label1.Click
+        PictureBox1.Image = Project_1.My.Resources.Resources.horse
+    End Sub
+
+    Private Sub Label2_MouseHover(sender As Object, e As EventArgs) Handles Label2.MouseHover
+        PictureBox1.Image = Project_1.My.Resources.Resources.flower
+    End Sub
+
+    Private Sub Label3_MouseHover(sender As Object, e As EventArgs) Handles Label3.MouseHover
+        PictureBox1.Image = Project_1.My.Resources.Resources.bird
+    End Sub
+End Class
